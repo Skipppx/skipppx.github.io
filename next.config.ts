af7eea8fe63b927ec373a8955437af888ae75529
Skipppx <kipdartfrog@gmail.com>
2025-05-21 00:21:54 +0100

    rebuild

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: 'out', // Specify the output directory
   // output: 'export',
   // basePath: process.env.PAGES_BASE_PATH || '',
 };
