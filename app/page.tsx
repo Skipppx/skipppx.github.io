a347732c4780e5a8de45931383eea584454a32b9
Skipppx <kipdartfrog@gmail.com>
2025-05-21 12:59:14 +0100

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -47,13 +47,13 @@ const readDataFromFile = (data: ArrayBuffer) => {
 };
 
 export default async function Home() {
-  const responseblob = await list();
-  {responseblob.blobs.map((blob: { pathname: boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | Promise<AwaitedReactNode> | Key | null | undefined; downloadUrl: string | undefined; }) => (
-    <a key={String(blob.pathname)} href={blob.downloadUrl}>
-      {blob.pathname}
-    </a>
-  ))}
-  console.log(responseblob);
+  // const responseblob = await list();
+  // {responseblob.blobs.map((blob: { pathname: boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | Promise<AwaitedReactNode> | Key | null | undefined; downloadUrl: string | undefined; }) => (
+  //   <a key={String(blob.pathname)} href={blob.downloadUrl}>
+  //     {blob.pathname}
+  //   </a>
+  // ))}
+  // console.log(responseblob);
   const [granboard, setGranboard] = useState<Granboard>();
   const [connectionState, setConnectionState] = useState<
     "Click Here To Connect" | "Connecting..." | "Connected" | "Error - please click to retry."
