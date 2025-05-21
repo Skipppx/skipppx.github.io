35c07e77ad5400760d22c96cbc968e451d1371c6
Skipppx <kipdartfrog@gmail.com>
2025-05-21 13:53:48 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
+module.exports = {
+  env: {
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+  },
+};
+
 const nextConfig: NextConfig = {
   // distDir: 'out', // Specify the output directory
   // output: 'export',
